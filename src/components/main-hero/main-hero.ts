174cd56937126cc9575fead84fdb2d4701d5c29c
regina <rmyachina@mail.ru>
2025-10-03 17:42:05 +0300

    feat: main-hero

--- src/components/main-hero/main-hero.ts
+++ src/components/main-hero/main-hero.ts
@@ -1,8 +1,31 @@
 import Swiper from 'swiper'
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules'
 
 void (function () {
     const mainHeroSlider = document.querySelector<HTMLElement>('.main-hero.swiper')
-    console.log(mainHeroSlider)
     if (!mainHeroSlider) return
-    new Swiper(mainHeroSlider, {})
+    new Swiper(mainHeroSlider, {
+        modules: [Autoplay, EffectFade, Pagination, Navigation],
+        effect: 'fade',
+        slidesPerView: 1,
+        loop: true,
+        fadeEffect: {
+            crossFade: true,
+        },
+        autoplay: {
+            delay: 3000,
+        },
+        allowTouchMove: false,
+        pagination: {
+            el: '.main-hero__pagination',
+            clickable: true,
+            bulletClass: 'main-hero__pagination-square',
+            bulletActiveClass: '_active',
+        },
+
+        navigation: {
+            prevEl: '.main-hero__navigation-prev',
+            nextEl: '.main-hero__navigation-next',
+        },
+    })
 })()
