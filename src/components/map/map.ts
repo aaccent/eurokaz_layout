57d8aafcd8b8bb6e2a67322b187c61664958f74e
Sevastyan <seva@aaccent.ru>
2025-11-13 16:05:45 +0300

    fix: add map styles and scripts

---
+++ src/components/map/map.ts
@@ -0,0 +1,37 @@
+import { isMobile } from 'globals/adaptive'
+import { gsap } from 'gsap'
+import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin'
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (isMobile) {
+        const mapSvg = document.querySelector<HTMLElement>('.map__svg-mobile svg')
+        const scalePlusButton = document.querySelector('.map__scale-plus')
+        const scaleMinusButton = document.querySelector('.map__scale-minus')
+
+        if (!mapSvg || !scaleMinusButton || !scaleMinusButton) return
+
+        let scale = 1
+
+        mapSvg.style.transform = `scale(${scale})`
+
+        scalePlusButton?.addEventListener('click', () => {
+            if (scale >= 3) return
+            scale += 0.2
+            mapSvg.style.transform = `scale(${scale})`
+        })
+
+        scaleMinusButton.addEventListener('click', () => {
+            if (scale === 1) return
+            scale -= 0.2
+            mapSvg.style.transform = `scale(${scale})`
+        })
+    }
+
+    gsap.registerPlugin(DrawSVGPlugin)
+
+    const tl = gsap.timeline({ repeat: -1, yoyo: true, repeatDelay: 1 })
+    tl.fromTo('.to-kz__anim', { drawSVG: '0' }, { duration: 2, drawSVG: true })
+    tl.fromTo('#kazakhstan', { filter: 'none' }, { filter: 'drop-shadow(0 0 12px rgba(123, 28, 25, 0.9))' })
+    tl.fromTo('.from-kz__anim', { drawSVG: '0' }, { duration: 2, drawSVG: true })
+    tl.fromTo('.dashed-lines', { opacity: 0 }, { duration: 2, opacity: 1 })
+})
